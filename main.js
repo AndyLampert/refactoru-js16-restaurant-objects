41d3d266b9db64a03ed0087c6c3dbeb935cac35e
runningtalus <dave@davemay.org>
2014-04-22 17:30:07 -0600

    edited syntax

--- main.js
+++ main.js
@@ -14,25 +14,25 @@ var Plate = function(name, description, price, ingredients) {
 	this.description = description;
 	this.price = price;
 	this.ingredients = ingredients;
-}
+};
 
 var Order =  function(arrayOfPlates) {
 	this.arrayOfPlates = arrayOfPlates;
-}
+};
 
 var Menu = function(arrayOfPlates) {
 	this.arrayOfPlates = arrayOfPlates;
-}
+};
 
 var Restaurant = function(name, description, Menu) {
 	this.name = name;
 	this.description = description;
 	this.Menu = Menu;
-}
+};
 
 var Customer = function(dietaryPreference) {
 	this.dietaryPreference = dietaryPreference;
-}
+};
 
 Fooditem.prototype.toString = function() {
 	var foodItemInfo = new String();
