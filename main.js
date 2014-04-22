be22eb650eedfda291a793e10de9c5e27e078fe2
runningtalus <dave@davemay.org>
2014-04-22 17:29:44 -0600

    created constructors for part 2 of exercise

--- main.js
+++ main.js
@@ -7,6 +7,33 @@ var Fooditem = function(name,calories,vegan,glutenFree,citrusFree){
 	this.citrusFree = citrusFree;
 };
 
+// part 2 of exercise
+// creating constructors
+var Plate = function(name, description, price, ingredients) {
+	this.name = name;
+	this.description = description;
+	this.price = price;
+	this.ingredients = ingredients;
+}
+
+var Order =  function(arrayOfPlates) {
+	this.arrayOfPlates = arrayOfPlates;
+}
+
+var Menu = function(arrayOfPlates) {
+	this.arrayOfPlates = arrayOfPlates;
+}
+
+var Restaurant = function(name, description, Menu) {
+	this.name = name;
+	this.description = description;
+	this.Menu = Menu;
+}
+
+var Customer = function(dietaryPreference) {
+	this.dietaryPreference = dietaryPreference;
+}
+
 Fooditem.prototype.toString = function() {
 	var foodItemInfo = new String();
 	return this.name + " has " + this.calories + " calories." + "\n" + 
