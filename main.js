d18f1137872ffc239f030c7fa7b08bb8a576ac7c
AndyLampert <lamperta@gmail.com>
2014-04-26 10:31:52 -0600

    added food items, plate items, and set up plate constructor

--- main.js
+++ main.js
@@ -7,14 +7,6 @@ var Fooditem = function(name,calories,vegan,glutenFree,citrusFree){
 	this.citrusFree = citrusFree; // also just called 'properties'
 };
 
-// Fooditem.prototype.toString = function() {
-// 	// var foodItemInfo = new String();
-// 	// no calories var so we can just say this.calories directly
-// 	return this.name + " has " + this.calories + " calories";
-// 	// add other dietary info
-// 	// when it uses type coercion tries to convert it to a string, it runs toString method internally
-// };
-
 var Food = function(name,calories,vegan,glutenFree,citrusFree) {
 	// calls the superclass's constructor within the context of the current instance
 	// ([first argument is the value of 'this'], arguments passed to function calling...)
@@ -25,25 +17,56 @@ Food.prototype = new Fooditem(); // inheritance step 2
 Food.prototype.constructor = Fooditem; // inheritance step 3
 
 // part 2 of exercise
-// creating constructors
+// creating constructors, setting properties of plate item
 var Plate = function(name, description, price, ingredients) {
 	this.name = name;
 	this.description = description;
 	this.price = price;
+	// our list of food items. we want to loop through this.
 	this.ingredients = ingredients;
+
+	// go through each item that we're passing and create the missing data
+	this.calories = 0;
+	this.glutenFree = true;
+
+	// item = whatever we're looping over. Arbitrary param name.
+	ingredients.forEach(function(ingredientParam){
+		this.calories += ingredientParam.calories;
+		
+		// if input param is NOT glutenfree
+		// if one of ANY of the items is not gluten free, then the whole plate is NOT gluten free
+		if (!ingredientParam.glutenFree) {
+			this.glutenFree = false; // if the ingredientParam IS NOT glutenfree, make the Plate also not GlutenFree
+		}
+		if (!ingredientParam.vegan) {
+			this.vegan = false;
+		}
+		if (!ingredientParam.citrusFree){
+			this.citrusFree = false;
+		}
+	});
 };
+
+// to make it readable, turning obj to a string?
+// turning our constructors to strings
+Fooditem.prototype.toString = function() {
+	var foodItemInfo = new String();
+	return this.name + " has " + this.calories + " calories." + "\n" + 
+	this.name + " is vegan? " + this.checkIfVegan() + "\n" + 
+	this.name + " is gluten free? " + this.checkIfGlutenFree() + "\n" + 
+	this.name + " is citrus free? " + this.checkIfCitrusFree() + "\n"; 
+};
+
 Plate.prototype.toString = function() {
-	
 	var plateItem = new String();
 	return this.name;
-
-		// var foodItemInfo = new String();
-		// return this.name + " has " + this.calories + " calories." + "\n" + 
-		// this.name + " is vegan? " + this.checkIfVegan() + "\n" + 
+	// var foodItemInfo = new String();
+	// return this.name + " has " + this.calories + " calories." + "\n" + 
+	this.name + " is vegan? " + this.checkIfVegan() + "\n";
 
 };
 
-var Order =  function(arrayOfPlates) {
+var Order = function(arrayOfPlates) {
 	this.arrayOfPlates = arrayOfPlates;
 };
 
@@ -77,14 +100,6 @@ Customer.prototype.toString = function() {
 
 };
 
-Fooditem.prototype.toString = function() {
-	var foodItemInfo = new String();
-	return this.name + " has " + this.calories + " calories." + "\n" + 
-		this.name + " is vegan? " + this.checkIfVegan() + "\n" + 
-		this.name + " is gluten free? " + this.checkIfGlutenFree() + "\n" + 
-		this.name + " is citrus free? " + this.checkIfCitrusFree() + "\n"; 
-};
-
 Fooditem.prototype.checkIfVegan = function(){
 	if(this.vegan){
 		return "Yes!";
@@ -112,6 +127,43 @@ Fooditem.prototype.checkIfCitrusFree = function(){
 	}
 };
 
+// creating food items. will pass in as ingredients for plates
+// params are ('name', calories, vegan, glutenFree, CitrusFree)
 var steak = new Fooditem('Steak', 400, false, true, true);
 var bread = new Fooditem('Bread', 80, true, false, true);
 var orange = new Fooditem('Orange', 45, true, true, false);
+var tortilla = new Fooditem('Tortilla', 100, true, false, true);
+var lettuce = new Fooditem('Lettuce', 20, true, true, true);
+var radish = new Fooditem('Radish', 30, true, true, true);
+var garlic = new Fooditem('Garlic', 1, true, true, true);
+var avocado = new Fooditem('Avocado', 80, true, true, true);
+var apple = new Fooditem('Apple', 35, true, true, false);
+var cheese = new Fooditem('Cheese', 150, false, true, true);
+var beans = new Fooditem('Black beans', 80, true, true, true);
+var lime = new Fooditem('lime', 10, true, true, false);
+var tomato = new Fooditem('sliced tomato', 15, true, true, true);
+
+// creating plates. these will make up our menu
+var veggieBurrito = new Plate('non-meaty burrito', 'burrito with no meat', [tortilla, lettuce, garlic, avocado, cheese, lime]);
+var steakBurrito = new Plate('Burrito', 'Who doesn\'t love burritos?', 10.00, [steak, tortilla, lettuce, garlic, avocado, cheese, lime]);
+var burritoBowl = new Plate('Burrito Bowl', 'Burrito without the gluten bs', 8.00, [steak, lettuce, garlic, avocado, cheese]);
+var salad = new Plate('Simple Salad', 'just veggies', 6.00, [lettuce, radish, cheese]);
+var steakSalad = new Plate('Steaky Salad', 'Meaty salad', 12.00, [steak, lettuce, garlic, cheese, radish]);
+var fruitSalad = new Plate('Fruit Salad' [lettuce, radish, orange, apple]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
