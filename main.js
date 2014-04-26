36e650e390b71fbaa0536d9a88c15961b94c5394
AndyLampert <lamperta@gmail.com>
2014-04-26 09:08:59 -0600

    sync

--- main.js
+++ main.js
@@ -3,28 +3,33 @@ var Fooditem = function(name,calories,vegan,glutenFree,citrusFree){
 	this.name = name;
 	this.calories = calories;
 	this.vegan = vegan;
-	this.glutenFree = glutenFree;
-	this.citrusFree = citrusFree;
+	this.glutenFree = glutenFree; // instance variables or member variables
+	this.citrusFree = citrusFree; // also just called 'properties'
 };
 
-Fooditem.prototype.toString = function() {
-	var foodItemInfo = new String();
-	this.name + " has " + calories + " calories";
-	return foodItemInfo;
+// Fooditem.prototype.toString = function() {
+// 	// var foodItemInfo = new String();
+// 	// no calories var so we can just say this.calories directly
+// 	return this.name + " has " + this.calories + " calories";
+// 	// add other dietary info
+// 	// when it uses type coercion tries to convert it to a string, it runs toString method internally
+// };
+
+
+
+
+var Food = function(name,calories,vegan,glutenFree,citrusFree) {
+	// calls the superclass's constructor within the context of the current instance
+	// ([first argument is the value of 'this'], arguments passed to function calling...)
+	Fooditem.call(this,name,calories,vegan,glutenFree,citrusFree); // inheritance step 1
 };
 
-var Food = function() {
-	
-};
-
-Food.prototype = new Fooditem();
-Food.prototype.constructor = Fooditem;
+Food.prototype = new Fooditem(); // inheritance step 2
+Food.prototype.constructor = Fooditem; // inheritance step 3
 
 var veggieWrap = new Food('Vegetable Wrap', 200, true, false, true);
 var pancakes = new Food('Pancakes', 300, false, false, true);
 var orange = new Food('Orange', 50, true, true, false);
-
-
 
 // current objective
 // Define a toString method on the constructor's prototype. 
